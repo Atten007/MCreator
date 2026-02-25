Atten007/MCreator

commit 921836d304d92115c9e8e773324acea86c0687aa

Add Block and Entity placer components to BEItems (#6076)
* Add Block and Entity placers components to BEItems

* Better texts

* Change entityToPlace to a JSingleEntrySelector field and remove the enable checbox + change UI

* Update tests

* Some minor changes

* Nom more annotations for single ME

---------

Co-authored-by: KlemenDEV <16374228+KlemenDEV@users.noreply.github.com>

plugins/generator-addon-1.21.x/addon-1.21.x/templates/item.json.ftl
@@ -28,6 +28,31 @@
       <#if data.stackSize lt 64>
       "minecraft:max_stack_size": ${data.stackSize},
       </#if>
+      <#if !data.blockToPlace.isEmpty()>
+      "minecraft:block_placer": {
+          "block": "${mappedMCItemToRegistryNameNoTags(data.blockToPlace)}",
+          "use_on": [
+            <#list data.blockPlaceableOn as block>
+            "${mappedMCItemToRegistryNameNoTags(block)}"<#sep>,
+            </#list>
+          ]
+      },
+      </#if>
+      <#if !data.entityToPlace.isEmpty()>
+      "minecraft:entity_placer": {
+        "entity": "${generator.map(data.entityToPlace.getUnmappedValue(), "entities")}",
+        "dispense_on": [
+            <#list data.entityDispensableOn as block>
+            "${mappedMCItemToRegistryNameNoTags(block)}"<#sep>,
+            </#list>
+        ],
+        "use_on": [
+            <#list data.entityPlaceableOn as block>
+            "${mappedMCItemToRegistryNameNoTags(block)}"<#sep>,
+            </#list>
+        ]
+      },
+      </#if>
       <#if data.isFood>
       "minecraft:use_modifiers": {
       	"use_duration": ${data.useDuration},

plugins/mcreator-localization/lang/texts.properties
@@ -3417,6 +3417,13 @@ elementgui.beitem.should_despawn=Should item despawn when dropped?
 elementgui.beitem.movement_modifier=Player movement modifier:
 elementgui.beblock.model=<html>Block model:<br><small>Supported: Built-in, Bedrock
 elementgui.beblock.sound_on_step=Sound on step:
+elementgui.beitem.block_to_place=Block to place on right-click:
+elementgui.beitem.placeable_on=<html>Can be placed on:<br>Leave empty to allow placing on all blocks
+elementgui.beitem.block_placer_properties=Block placer properties
+elementgui.beitem.entity_to_place=<html>Entity to spawn/dispense:<br>Leave empty to disable
+elementgui.beitem.entity_dispensable_on=<html>Can be dispensed on:<br>Leave empty allow dispensing on all blocks
+elementgui.beitem.entity_placeable_on=<html>Can be spawned on:<br>Leave empty allow spawning on all blocks
+elementgui.beitem.entity_placer_properties=Entity placer properties
 tab.workspace=Workspace
 tab.image_maker=Image maker
 tab.animation_maker=Animation maker

src/main/java/net/mcreator/element/types/bedrock/BEItem.java
@@ -20,6 +20,7 @@
 package net.mcreator.element.types.bedrock;
 
 import net.mcreator.element.GeneratableElement;
+import net.mcreator.element.parts.EntityEntry;
 import net.mcreator.element.parts.MItemBlock;
 import net.mcreator.element.parts.TextureHolder;
 import net.mcreator.element.types.interfaces.IItem;
@@ -32,6 +33,7 @@ import net.mcreator.workspace.references.ModElementReference;
 import net.mcreator.workspace.references.TextureReference;
 
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BEItem extends GeneratableElement implements IItem, IItemWithTexture {
@@ -54,6 +56,11 @@ public class BEItem extends GeneratableElement implements IItem, IItemWithTextur
 	public boolean allowOffHand;
 	public double fuelDuration;
 	public boolean shouldDespawn;
+	public MItemBlock blockToPlace;
+	@ModElementReference public List<MItemBlock> blockPlaceableOn;
+	public EntityEntry entityToPlace;
+	@ModElementReference public List<MItemBlock> entityPlaceableOn;
+	@ModElementReference public List<MItemBlock> entityDispensableOn;
 
 	// Food
 	public boolean isFood;
@@ -75,6 +82,9 @@ public class BEItem extends GeneratableElement implements IItem, IItemWithTextur
 		this.animation = "eat";
 		this.enableCreativeTab = true;
 		this.creativeTab = "MATERIALS";
+		this.blockPlaceableOn = new ArrayList<>();
+		this.entityDispensableOn = new ArrayList<>();
+		this.entityPlaceableOn = new ArrayList<>();
 	}
 
 	@Override public BufferedImage generateModElementPicture() {

src/main/java/net/mcreator/ui/minecraft/SingleSpawnableEntitySelector.java
@@ -1,0 +1,40 @@
+/*
+ * MCreator (https://mcreator.net/)
+ * Copyright (C) 2012-2020, Pylo
+ * Copyright (C) 2020-2026, Pylo, opensource contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.mcreator.ui.minecraft;
+
+import net.mcreator.element.parts.EntityEntry;
+import net.mcreator.minecraft.ElementUtil;
+import net.mcreator.ui.MCreator;
+import net.mcreator.ui.component.JSingleEntrySelector;
+import net.mcreator.ui.dialogs.DataListSelectorDialog;
+import net.mcreator.ui.init.L10N;
+
+public class SingleSpawnableEntitySelector extends JSingleEntrySelector<EntityEntry> {
+
+	public SingleSpawnableEntitySelector(MCreator mcreator) {
+		super(mcreator);
+	}
+
+	@Override protected EntityEntry openEntrySelector() {
+		var entry = DataListSelectorDialog.openSelectorDialog(mcreator, ElementUtil::loadAllSpawnableEntities,
+				L10N.t("dialog.selector.title"), L10N.t("dialog.selector.entity.message"));
+		return entry == null ? null : new EntityEntry(mcreator.getWorkspace(), entry);
+	}
+}

src/main/java/net/mcreator/ui/modgui/bedrock/BEItemGUI.java
@@ -29,9 +29,7 @@ import net.mcreator.ui.component.util.PanelUtils;
 import net.mcreator.ui.dialogs.TypedTextureSelectorDialog;
 import net.mcreator.ui.help.HelpUtils;
 import net.mcreator.ui.init.L10N;
-import net.mcreator.ui.minecraft.DataListComboBox;
-import net.mcreator.ui.minecraft.MCItemHolder;
-import net.mcreator.ui.minecraft.TextureSelectionButton;
+import net.mcreator.ui.minecraft.*;
 import net.mcreator.ui.modgui.ModElementGUI;
 import net.mcreator.ui.validation.ValidationGroup;
 import net.mcreator.ui.validation.component.VTextField;
@@ -67,15 +65,21 @@ public class BEItemGUI extends ModElementGUI<BEItem> {
 	private final JCheckBox enableCreativeTab = new JCheckBox();
 	private final DataListComboBox creativeTab = new DataListComboBox(mcreator,
 			ElementUtil.loadAllTabs(mcreator.getWorkspace()));
-	private final JCheckBox isHiddenInCommands = L10N.checkbox("elementgui.common.enable");
 	private final JSpinner maxDurability = new JSpinner(new SpinnerNumberModel(0, 0, 128000, 1));
 	private final JSpinner useDuration = new JSpinner(new SpinnerNumberModel(1.6, 0, 128000, 0.1));
 	private final JSpinner movementModifier = new JSpinner(new SpinnerNumberModel(0.35, 0, 1, 0.05));
 	private final JSpinner damageVsEntity = new JSpinner(new SpinnerNumberModel(0, 0, 128000, 0.1));
 	private final JCheckBox enableMeleeDamage = new JCheckBox();
+
+	private final JCheckBox isHiddenInCommands = L10N.checkbox("elementgui.common.enable");
 	private final JCheckBox allowOffHand = L10N.checkbox("elementgui.common.enable");
 	private final JSpinner fuelDuration = new JSpinner(new SpinnerNumberModel(0, 0, 107374180, 0.05));
 	private final JCheckBox shouldDespawn = L10N.checkbox("elementgui.common.enable");
+	private final MCItemHolder blockToPlace = new MCItemHolder(mcreator, ElementUtil::loadBlocks);
+	private final MCItemListField blockPlaceableOn = new MCItemListField(mcreator, ElementUtil::loadBlocks);
+	private final SingleSpawnableEntitySelector entityToPlace = new SingleSpawnableEntitySelector(mcreator);
+	private final MCItemListField entityDispensableOn = new MCItemListField(mcreator, ElementUtil::loadBlocks);
+	private final MCItemListField entityPlaceableOn = new MCItemListField(mcreator, ElementUtil::loadBlocks);
 
 	private final JCheckBox isFood = L10N.checkbox("elementgui.common.enable");
 	private final JSpinner foodNutritionalValue = new JSpinner(new SpinnerNumberModel(4, -1000, 1000, 1));
@@ -229,7 +233,44 @@ public class BEItemGUI extends ModElementGUI<BEItem> {
 		shouldDespawn.setOpaque(false);
 		advancedProperties.add(shouldDespawn);
 
-		advancedPanel.add("Center", PanelUtils.totalCenterInPanel(advancedProperties));
+		JPanel blockPlacerProps = new JPanel(new GridLayout(2, 2, 65, 2));
+		blockPlacerProps.setOpaque(false);
+
+		blockPlacerProps.add(HelpUtils.wrapWithHelpButton(this.withEntry("beitem/block_to_place"),
+				L10N.label("elementgui.beitem.block_to_place")));
+		blockToPlace.setOpaque(false);
+		blockPlacerProps.add(PanelUtils.centerInPanel(blockToPlace));
+		blockToPlace.addBlockSelectedListener(e -> updateBlockUsableOnList());
+
+		blockPlacerProps.add(HelpUtils.wrapWithHelpButton(this.withEntry("beitem/block_placeable_on"),
+				L10N.label("elementgui.beitem.placeable_on")));
+		blockPlaceableOn.setOpaque(false);
+		blockPlacerProps.add(blockPlaceableOn);
+
+		ComponentUtils.makeSection(blockPlacerProps, L10N.t("elementgui.beitem.block_placer_properties"));
+
+		JPanel entityPlacerProps = new JPanel(new GridLayout(3, 2, 65, 2));
+		entityPlacerProps.setOpaque(false);
+
+		entityPlacerProps.add(HelpUtils.wrapWithHelpButton(this.withEntry("beitem/entity_to_place"),
+				L10N.label("elementgui.beitem.entity_to_place")));
+		entityToPlace.setOpaque(false);
+		entityPlacerProps.add(entityToPlace);
+
+		entityPlacerProps.add(HelpUtils.wrapWithHelpButton(this.withEntry("beitem/entity_placeable_on"),
+				L10N.label("elementgui.beitem.entity_placeable_on")));
+		entityPlaceableOn.setOpaque(false);
+		entityPlacerProps.add(entityPlaceableOn);
+
+		entityPlacerProps.add(HelpUtils.wrapWithHelpButton(this.withEntry("beitem/entity_dispensable_on"),
+				L10N.label("elementgui.beitem.entity_dispensable_on")));
+		entityDispensableOn.setOpaque(false);
+		entityPlacerProps.add(entityDispensableOn);
+
+		ComponentUtils.makeSection(entityPlacerProps, L10N.t("elementgui.beitem.entity_placer_properties"));
+
+		advancedPanel.add("Center", PanelUtils.totalCenterInPanel(
+				PanelUtils.column(advancedProperties, blockPlacerProps, entityPlacerProps)));
 
 		page1group.addValidationElement(name);
 		page1group.addValidationElement(texture);
@@ -250,6 +291,11 @@ public class BEItemGUI extends ModElementGUI<BEItem> {
 		updateCreativeTab();
 		updateMeleeDamage();
 		updateFoodPanel();
+		updateBlockUsableOnList();
+	}
+
+	@Override public void reloadDataLists() {
+		super.reloadDataLists();
 	}
 
 	private void updateFoodPanel() {
@@ -280,6 +326,10 @@ public class BEItemGUI extends ModElementGUI<BEItem> {
 		creativeTab.setEnabled(enableCreativeTab.isSelected());
 	}
 
+	private void updateBlockUsableOnList() {
+		blockPlaceableOn.setEnabled(blockToPlace.containsItem());
+	}
+
 	@Override protected void openInEditingMode(BEItem item) {
 		texture.setTexture(item.texture);
 		name.setText(item.name);
@@ -304,9 +354,15 @@ public class BEItemGUI extends ModElementGUI<BEItem> {
 		shouldDespawn.setSelected(item.shouldDespawn);
 		usingConvertsTo.setBlock(item.usingConvertsTo);
 		animation.setSelectedItem(item.animation);
+		blockToPlace.setBlock(item.blockToPlace);
+		blockPlaceableOn.setListElements(item.blockPlaceableOn);
+		entityToPlace.setEntry(item.entityToPlace);
+		entityDispensableOn.setListElements(item.entityDispensableOn);
+		entityPlaceableOn.setListElements(item.entityPlaceableOn);
 		updateFoodPanel();
 		updateMeleeDamage();
 		updateCreativeTab();
+		updateBlockUsableOnList();
 	}
 
 	@Override public BEItem getElementFromGUI() {
@@ -334,6 +390,11 @@ public class BEItemGUI extends ModElementGUI<BEItem> {
 		item.shouldDespawn = shouldDespawn.isSelected();
 		item.usingConvertsTo = usingConvertsTo.getBlock();
 		item.animation = animation.getSelectedItem();
+		item.blockToPlace = blockToPlace.getBlock();
+		item.blockPlaceableOn = blockPlaceableOn.getListElements();
+		item.entityToPlace = entityToPlace.getEntry();
+		item.entityDispensableOn = entityDispensableOn.getListElements();
+		item.entityPlaceableOn = entityPlaceableOn.getListElements();
 
 		return item;
 	}

src/test/java/net/mcreator/integration/TestWorkspaceDataProvider.java
@@ -1692,6 +1692,20 @@ public class TestWorkspaceDataProvider {
 			beitem.animation = getRandomItem(random,
 					new String[] { "block", "bow", "crossbow", "drink", "eat", "none", "spear", "camera", "brush",
 							"spyglass" });
+			beitem.blockToPlace = new MItemBlock(modElement.getWorkspace(),
+					getRandomMCItem(random, filterAir(blocks)).getName());
+			beitem.blockPlaceableOn = new ArrayList<>();
+			beitem.entityToPlace = new EntityEntry(modElement.getWorkspace(), getRandomItem(random, ElementUtil.loadAllSpawnableEntities(modElement.getWorkspace())));
+			beitem.entityDispensableOn = new ArrayList<>();
+			beitem.entityPlaceableOn = new ArrayList<>();
+			if (!emptyLists) {
+				beitem.blockPlaceableOn = subset(random, blocks.size() / 8, blocks,
+						e -> new MItemBlock(modElement.getWorkspace(), e.getName()));
+				beitem.entityDispensableOn = subset(random, blocks.size() / 8, blocks,
+						e -> new MItemBlock(modElement.getWorkspace(), e.getName()));
+				beitem.entityPlaceableOn = subset(random, blocks.size() / 8, blocks,
+						e -> new MItemBlock(modElement.getWorkspace(), e.getName()));
+			}
 			return beitem;
 		} else if (ModElementType.BEBLOCK.equals(modElement.getType())) {
 			BEBlock block = new BEBlock(modElement);

commit 7c51aa54b2f6f63a452e0f6ea5e413451dcf135f

Fix bug introduced by #6076 where existing items would not work due to missing null checks

plugins/generator-addon-1.21.x/addon-1.21.x/templates/item.json.ftl
@@ -28,7 +28,7 @@
       <#if data.stackSize lt 64>
       "minecraft:max_stack_size": ${data.stackSize},
       </#if>
-      <#if !data.blockToPlace.isEmpty()>
+      <#if data.blockToPlace?? && !data.blockToPlace.isEmpty()>
       "minecraft:block_placer": {
           "block": "${mappedMCItemToRegistryNameNoTags(data.blockToPlace)}",
           "use_on": [
@@ -38,7 +38,7 @@
           ]
       },
       </#if>
-      <#if !data.entityToPlace.isEmpty()>
+      <#if data.entityToPlace?? && !data.entityToPlace.isEmpty()>
       "minecraft:entity_placer": {
         "entity": "${generator.map(data.entityToPlace.getUnmappedValue(), "entities")}",
         "dispense_on": [